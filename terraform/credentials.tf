312f922514622374e3b61de38bce4eefb8f8ecd4
fersevillano <75248115+fersevillano@users.noreply.github.com>
2022-03-14 20:13:14 +0100

    Borro credentials

--- terraform/credentials.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "45a46f14-9c58-4a21-a288-bf1379f4cf14"
-  client_id       = "2f97fb65-220c-4bf0-9bdb-7614db2583b9"
-  client_secret   = "ysQpEy8pRysZhm4qJAf5l.aAy3G16hk.95"
-  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
-}
